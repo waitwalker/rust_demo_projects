ec2e9042cc0e40d1f194f0e2da3de05d1343952a
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-15 16:06:00 +0800

    feat: 添加条件语句和模式匹配示例
    - 添加 if-else if-else 条件语句示例
    - 添加 match 模式匹配语句示例
    - 展示两种不同的条件判断方式
    - 修复了之前不完整的代码结构

--- function/src/main.rs
+++ function/src/main.rs
@@ -20,11 +20,19 @@ fn sum() {
 
     println!("The value of y is {}", y);
 
-    /**
-     * 
-     * 
-     * 
-    */
+    if y > 5 {
+        println!("The value of x is greater than 5");
+    } else if y == 5 {
+        println!("The value of x is equal to 5");
+    } else {
+        println!("The value of x is less than 5");
+    }
+
+    match y {
+        n if n >  5 => println!("The value of x is greater than 5"),
+        5 => println!("The value of x is equal to 5"),
+        _ => println!("The value of x is less than 5"),
+    }
 }
 
 fn sumAdd(x: i64, y: i64) -> i64 {
