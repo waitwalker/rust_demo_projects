c59aedfe9a96704bc41f81735dc0e48988112967
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-15 18:03:10 +0800

    feat: 添加所有权移动和克隆示例
    - 添加整数类型的赋值示例（Copy trait）
    - 添加 String 类型的移动示例（Move 语义）
    - 展示移动后原变量失效的特性
    - 添加克隆操作示例（clone 方法）
    - 说明克隆会复制堆上的数据

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -10,4 +10,23 @@ fn main() {
     println!("The value of str1 is {}", str1);
     str1.push_str(", World!");
     println!("The value of str1 is {}", str1);
+    // 当变量走出作用域时，Rust会自动调用drop函数，释放变量占用的内存
+    let x = 5;
+    let y = x;
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
+
+    let s1 = String::from("hello");
+    // s1移动后，赋值给s2就失效了，s1就不能再使用了，此时s2是有效的
+    let s2 = s1;
+    // println!("The value of s1 is {}", s1);
+    println!("The value of s2 is {}", s2);
+
+    // 克隆操作会把Stack上的指针、长度、容量，以及Heap上的值都会复制一份
+    let s3 = s2.clone();
+    println!("The value of s2 is {}", s2);
+    println!("The value of s3 is {}", s3);
+
+
+
 }
