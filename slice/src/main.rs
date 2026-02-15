6b8bb60e6b3df052a579ee0e2228cf3bff7c2886
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-15 20:55:25 +0800

    feat: 添加字符串切片语法示例
    - 添加使用范围语法创建字符串切片的示例
    - 展示 [start..end] 语法指定切片范围
    - 展示 [start..] 语法从起始位置到字符串末尾
    - 演示如何获取字符串的子串

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     let mut s1 = String::from("hello world");
     let word = first_world(&s1);
     println!("The value of word is {}", word);
+
+    let hello = &s1[0..5];
+    
+    let world = &s1[6..];
+
+    println!("The value of hello is {}", hello);
+    println!("The value of world is {}", world);
 }
 
 fn first_world(s: &String) -> usize {
