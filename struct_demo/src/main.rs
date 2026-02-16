8d8716efba790d672c738ae023e2c8b613798cc1
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-16 12:39:44 +0800

    feat: 添加结构体字段访问和修改示例
    - 添加访问结构体字段的示例
    - 添加修改结构体字段的示例
    - 展示需要使用 mut 关键字才能修改结构体字段
    - 演示如何打印结构体的各个字段值

--- struct_demo/src/main.rs
+++ struct_demo/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("Hello, world!");
 
     // 实例化结构体
-    let user1 = User {
+    let mut user1 = User {
         username: String::from("LiKai"),
         email: String::from("zhang@gmail.com"),
         sign_in_count: 456,
@@ -17,4 +17,12 @@ fn main() {
     };
 
     // 访问结构体
+    println!(
+        "username:{}, email:{}, sign_in_count:{}, activie:{}",
+        user1.username, user1.email, user1.sign_in_count, user1.active
+    );
+
+    // 重新赋值,struct 必须可变，才能赋值
+    user1.email = String::from("123456@gmail.com");
+    println!("username:{}, email:{}, sign_in_count:{}, activie:{}",user1.username,user1.email,user1.sign_in_count,user1.active);
 }
