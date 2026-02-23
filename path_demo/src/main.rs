ad2add7400bc37fdea126bd000a8ee7bbfc6dec4
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-23 17:42:58 +0800

    feat: 添加依赖和导入示例
    - 添加 rand 0.8.5 依赖
    - 新增 rand::Rng 导入
    - 添加 std::hashmap::HashMap 导入
    - 添加嵌套路径导入示例 std::{hash::Hash, cmp::Eq}

--- path_demo/src/main.rs
+++ path_demo/src/main.rs
@@ -1,6 +1,10 @@
 use path_demo::color::rgb;
 use std::fmt::Result;
 use std::io::Result as IoResult;
+use rand::Rng;
+use std::hashmap::HashMap;
+// 引入嵌套路径
+use std::{hash::Hash, cmp::Eq};
 
 fn main() {
     println!("Hello, world!");
