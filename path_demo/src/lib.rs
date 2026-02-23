9b1f651f8bbd96af46d5583e6c29d7f8dde48c93
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-23 13:48:03 +0800

    feat: 添加 super 关键字和嵌套模块示例
    - 添加 serve_order 函数
    - 添加 back_of_house 模块演示嵌套模块
    - 添加 fix_incorrect_order 函数演示使用 super 访问父模块
    - 添加 cook_order 函数
    - 展示 super 关键字的使用（相当于文件系统中的 ..）
    - 展示在嵌套模块中使用绝对路径（crate::）

--- path_demo/src/lib.rs
+++ path_demo/src/lib.rs
@@ -12,3 +12,19 @@ pub fn eat_at_restaurant() {
     // 相对路径
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        // 相对路径：super相当于文件系统中的..
+        super::serve_order();
+        // 绝对路径
+        crate::serve_order();
+    }
+
+    fn cook_order() {
+        println!("cook order");
+    }
+}
