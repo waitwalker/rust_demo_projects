6fe2d465fc93cbfcd70c34ed7ef6a0251c127d26
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-24 12:38:24 +0800

    feat: 添加字符串操作示例
    - 添加 to_string() 方法示例
    - 添加 push_str() 方法示例
    - 添加字符串拼接示例

--- string_demo/src/main.rs
+++ string_demo/src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
     println!("Hello, world!");
     let mut s = String::from("hello");
+
+    let data = "initial contents";
+
+    let data_string = data.to_string();
+
+    let mut s = String::from("foo");
+    // String可以追加字符串切片
+    s.push_str("bar");
+    println!("{}",s);
+
+    let mut s1 = String::from("hello");
+    let s2 = String::from(" world");
+
+    s1.push_str(&s2);
+
+    println!("{}",s1);
+
+
+    
 }
