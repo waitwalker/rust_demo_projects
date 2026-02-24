8222870d3d93208f7b8ca62cbe40f8bbaecc87d4
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-24 16:00:56 +0800

    feat: 添加 HashMap 遍历、更新和插入示例
    - 将 new_scores 改为可变变量
    - 添加 for 循环遍历 HashMap 示例
    - 添加新的 class HashMap 示例
    - 添加 insert() 方法更新值示例
    - 添加 entry() 和 or_insert() 方法示例

--- hashmap_demo/src/main.rs
+++ hashmap_demo/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let teams = vec![String::from("Blue"), String::from("Yellow")];
     let initial_scores = vec![10, 50];
-    let new_scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    let mut new_scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
     println!("new scores {:?}", new_scores);
     let team_name = String::from("Blue");
 
@@ -20,5 +20,28 @@ fn main() {
     } else {
         println!("score: {}", 0);
     }
-    
+
+    for (key, value) in &new_scores {
+        println!("{}: {}", key, value);
+    }
+
+    let mut class: HashMap<String, String> = HashMap::new();
+    class.insert(String::from("张三"), String::from("100"));
+    class.insert(String::from("李四"), String::from("90"));
+    class.insert(String::from("王五"), String::from("80"));
+    println!("{:?}", class);
+
+    class.insert(String::from("张三"), String::from("200"));
+    for (key, value) in &class {
+        println!("key {}: value{}", key, value);
+    }
+
+    class
+        .entry(String::from("张三"))
+        .or_insert(String::from("12000"));
+    class.entry(String::from("忘记")).or_insert(String::from("09876"));
+
+    for (key, value) in &class {
+        println!("key {}: value {}", key, value);
+    }
 }
