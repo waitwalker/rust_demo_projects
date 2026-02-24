bf8e7fcdb5b2e7d627ac0d1c260c5fba99fad5bd
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-24 14:51:36 +0800

    feat: 添加 HashMap 从向量创建示例
    - 添加使用 zip() 和 collect() 方法从向量创建 HashMap 示例
    - 优化代码格式和缩进

--- hashmap_demo/src/main.rs
+++ hashmap_demo/src/main.rs
@@ -3,7 +3,12 @@ use std::collections::HashMap;
 fn main() {
     println!("Hello, world!");
 
-    let mut scores:HashMap<String,i32> = HashMap::new();
-    scores.insert(String::from("张三"),100);
-    println!("{:?}",scores);
+    let mut scores: HashMap<String, i32> = HashMap::new();
+    scores.insert(String::from("张三"), 100);
+    println!("{:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let new_scores: HashMap<String, i32> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("new scores {:?}", new_scores);
 }
