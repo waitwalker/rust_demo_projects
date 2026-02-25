2e549e83d43fbc4daa5103d829007a4c9cdf2bb7
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-25 08:53:54 +0800

    feat: 添加读取文件内容的函数
    - 添加 std::io::Read 和 std::io 导入
    - 添加 read_username_from_file() 函数
    - 使用 match 表达式处理文件打开和读取错误
    - 返回 Result<String, io::Error> 类型

--- string_demo/src/main.rs
+++ string_demo/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::Read;
+use std::io;
+
 fn main() {
     println!("Hello, world!");
     // panic!("crash and burn");
@@ -76,6 +79,23 @@ fn main() {
 
     // let f = File::open("hello.txt").unwrap();
 
-    let f = File::open("hello.txt").expect("无法打开文件 hello.txt");
+    // let f = File::open("hello.txt").expect("无法打开文件 hello.txt");
+
+    let result = read_username_from_file();
     
+}
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
