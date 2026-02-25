89400a62121b29bc2457ecf3e8f0216bd38aff6c
chuanan.liu <chuanan.liu@sistalk.com>
2026-02-25 08:27:16 +0800

    feat: 添加文件操作和错误处理示例
    - 添加 std::fs::File 和 std::io::ErrorKind 导入
    - 注释掉向量越界访问代码
    - 添加 File::open() 和 File::create() 示例
    - 添加嵌套 match 表达式处理文件错误
    - 添加 hello.txt 测试文件
    - 优化代码格式

--- string_demo/src/main.rs
+++ string_demo/src/main.rs
@@ -1,8 +1,10 @@
+use std::fs::File;
+use std::io::ErrorKind;
 fn main() {
     println!("Hello, world!");
     // panic!("crash and burn");
-    let v = vec![1,3,5];
-    println!("value is {}",v[10]);
+    // let v = vec![1,3,5];
+    // println!("value is {}",v[10]);
 
     let mut s = String::from("hello");
 
@@ -13,29 +15,28 @@ fn main() {
     let mut s = String::from("foo");
     // String可以追加字符串切片
     s.push_str("bar");
-    println!("{}",s);
+    println!("{}", s);
 
     let mut s1 = String::from("hello");
     let s2 = String::from(" world");
 
     s1.push_str(&s2);
 
-    println!("{}",s1);
+    println!("{}", s1);
 
     let x1 = String::from("hello");
     let x2 = String::from("world");
     let x3 = x1 + &x2;
-    println!("{}",x3);
+    println!("{}", x3);
     // + 类似于 add(selft,s:&str)函数，x1的所有权移到函数内部了
     // println!("{}",x1);
-    println!("{}",x2);
+    println!("{}", x2);
 
-
-    let x8 = format!("{}-{}-{}","hello","world","rust");
-    println!("{}",x8);
+    let x8 = format!("{}-{}-{}", "hello", "world", "rust");
+    println!("{}", x8);
 
     let x9 = String::from("你好String");
-    println!("length:{}",x9.len());
+    println!("length:{}", x9.len());
     // println!("{}",x9[0]);
 
     for elment in x9.bytes() {
@@ -47,7 +48,18 @@ fn main() {
     }
 
     let x10 = &x9[0..3];
-    println!("{:?}",x10);
+    println!("{:?}", x10);
 
-    
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file: {:?}", e),
+            },
+            other_error => panic!("Error opening file: {:?}", other_error),
+        },
+    };
 }
