9776680f66a8c76cf13b41d255eb688aaa81f81f
chuanan.liu <chuanan.liu@sistalk.com>
2026-04-27 21:07:10 +0800

    add web demo

---
+++ rust_pg_diesel_actix/src/scheme.rs
@@ -0,0 +1,11 @@
+use diesel::table;
+table! {
+    employee(id) {
+        id -> Int4,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        department -> Varchar,
+        salary -> Int4,
+        age -> Int4,
+    }
+}
